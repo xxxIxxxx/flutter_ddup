0036781dd03cead82fe5ab70e8b0ac4c4bac40b8
Z <>
2022-03-31 22:38:07 +0800

    路径修改

--- lib/route/route_handlers.dart
+++ lib/route/route_handlers.dart
@@ -5,22 +5,22 @@ import '../class/Align,Center对齐与相对定位/align_center_page.dart';
 import '../class/Clip剪切组件/clip_page.dart';
 import '../class/Container组件/container_page.dart';
 import '../class/DecoratedBox装饰容器渐变边框等/decorated_box_page.dart';
+import '../class/ElevatedButton,TextButton,OutlinedButton,IconButton按钮/button_page.dart';
 import '../class/Flex,Expanded,Spacer弹性布局/flex_expanded_page.dart';
+import '../class/Image图片/image_page.dart';
 import '../class/LayoutBuilder获取父组件传递的约束信息/layout_builder_page.dart';
+import '../class/LinearProgressIndicator,CircularProgressIndicator,Timer进度指示器/progress_page.dart';
 import '../class/Padding填充/padding_page.dart';
+import '../class/Row,Column线性布局/row_page.dart';
 import '../class/Stack,Positioned层叠布局/stack_positioned_page.dart';
+import '../class/Switch,Checkbox单选开关复选开关/switch_page.dart';
+import '../class/TextField,Form输入框及表单/text_field_page.dart';
 import '../class/Text文本及样式/text_page.dart';
 import '../class/Transform,RotatedBox变换旋转缩放/transform_rotated_box_page.dart';
 import '../class/UnconstrainedBox,ConstrainedBox,SizedBox约束布局/constraints_page.dart';
 import '../class/Warp,Flow流式布局/warp_flow_page.dart';
 import '../class/widget生命周期/counter_life_cycle_page.dart';
-import '../class/单选开关复选开关/switch_page.dart';
-import '../class/图片/image_page.dart';
-import '../class/按钮/button_page.dart';
-import '../class/线性布局Row,Column/row_page.dart';
 import '../class/路由传值/tipRoute_page.dart';
-import '../class/输入框及表单/text_field_page.dart';
-import '../class/进度指示器/progress_page.dart';
 
 var homeHandler = Handler(
     handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
